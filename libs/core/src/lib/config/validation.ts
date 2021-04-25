c5a26f71ef4b38bfd253591489b04c348a9a519e
Faisal Mahmood <dfaisalmahmood@gmail.com>
2021-04-25 08:15:08 +0600

    feat: initial setup for typeorm and postgres

--- libs/core/src/lib/config/validation.ts
+++ libs/core/src/lib/config/validation.ts
@@ -3,4 +3,10 @@ import Joi = require("joi");
 export const validationSchema = Joi.object({
   NODE_ENV: Joi.string().valid("development", "production", "test").required(),
   PORT: Joi.number().default(3000),
+  // Database
+  DB_HOST: Joi.string().default("localhost"),
+  DB_PORT: Joi.number().default(5432),
+  DB_DATABASE: Joi.string().default("postgres"),
+  DB_USERNAME: Joi.string().default("postgres"),
+  DB_PASSWORD: Joi.string().required(),
 });
--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,20 +1,36 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
-import { configuration } from "./config/configuration";
+import coreConfig from "./config/core.config";
 import { CoreResovler } from "./config/core.resolver";
 import { validationSchema } from "./config/validation";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import databaseConfig from "./config/database.config";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration],
+      load: [coreConfig, databaseConfig],
       validationSchema,
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       playground: true, // TODO: Change to "false" during actual production
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        host: config.get("database.host"),
+        port: config.get("database.port"),
+        database: config.get("database.name"),
+        username: config.get("database.username"),
+        password: config.get("database.password"),
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        synchronize: config.get("environment") === "production" ? false : true,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
